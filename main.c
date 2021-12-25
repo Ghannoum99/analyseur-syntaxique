631763451016071e2801a739a0f3cd2cf4f0df70
Ghannnoum99 <jihad.ghannoum@ens.uvsq.fr>
2021-12-25 11:48:42 +0100

    update

--- main.c
+++ main.c
@@ -14,15 +14,19 @@
  // Ajouter des commentaires
 
 
-int main()
+int main(int argc, char* argv[])
 {
-	file_read fr = read_file("./tests/test5");
+	if (argc < 3)
+	{
+		fprintf(stderr,"L'analyseur prend 3 arguments\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	file_read fr = read_file(argv[1]);
 	print_grammar(fr.G);
 	print_table(fr.t, fr.G);
 	
 	printf("\n");
 	
-	build_tree_analysis(fr.G, fr.t, "aaabbbb", 7);
-	
-	return 0;
+	build_tree_analysis(fr.G, fr.t, argv[2],5);
 }
